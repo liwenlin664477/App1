ed3770884f6fc2894f6b0a8c3cc39f9ab50f0f46
liwenlin <u1327012@utah.edu>
2023-02-14 16:18:27 -0700

    Implemented the two activities interaction

--- app/src/main/java/com/App1/myapplication/MainActivity.kt
+++ app/src/main/java/com/App1/myapplication/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.App1.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
 import android.os.Bundle
+import android.widget.Toast
+import android.content.Intent
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var firstName: String? = null
+    private var lastName: String? = null
+    private var submitButton: Button? = null
+    private var viewFirstName: EditText? = null
+    private var viewLastName: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        submitButton = findViewById(R.id.button)
+        submitButton!!.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button -> {
+
+                //First, get the string from the EditText
+                viewFirstName = findViewById(R.id.firstName)
+                viewLastName = findViewById(R.id.lastName)
+                firstName = viewFirstName!!.text.toString()
+                lastName = viewLastName!!.text.toString()
+
+
+                //Check if the EditText string is empty
+                if (firstName.isNullOrBlank() || lastName.isNullOrBlank()) {
+                    //Complain that there's no text
+                    Toast.makeText(this@MainActivity, "Enter the first name or last name!", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    //Reward them for submitting their names
+                    Toast.makeText(this@MainActivity, "Good job!", Toast.LENGTH_SHORT).show()
+
+                    //Start an activity and pass the EditText string to it.
+                    val messageIntent = Intent(this, MainActivity2::class.java)
+                    messageIntent.putExtra("First", firstName)
+                    messageIntent.putExtra("Last", lastName)
+                    this.startActivity(messageIntent)
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/App1/myapplication/MainActivity2.kt
+++ app/src/main/java/com/App1/myapplication/MainActivity2.kt
@@ -1,11 +1,27 @@
 package com.App1.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
+    private var viewLogin: TextView? = null
+    private var firstName: String? = null
+    private var lastName: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        var Intent = intent
+        firstName = Intent.getStringExtra("First")
+        lastName = Intent.getStringExtra("Last")
+        if (firstName.isNullOrBlank() || lastName.isNullOrBlank()) {
+            Toast.makeText(this@MainActivity2, "Empty string received", Toast.LENGTH_SHORT).show()
+        } else {
+            //Get the text views where we will display names
+            viewLogin = findViewById(R.id.login)
+            viewLogin!!.text = firstName + " " + lastName + " is logged in!"
+        }
     }
 }
