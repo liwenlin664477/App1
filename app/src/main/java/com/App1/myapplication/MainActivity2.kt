3f15b177e9c0c8fe1091842539e6c9d710769dd1
liwenlin <u1327012@utah.edu>
2023-02-19 22:54:35 -0700

    Implemented the rotation

--- app/src/main/java/com/App1/myapplication/MainActivity2.kt
+++ app/src/main/java/com/App1/myapplication/MainActivity2.kt
@@ -10,18 +10,34 @@ class MainActivity2 : AppCompatActivity() {
     private var viewLogin: TextView? = null
     private var firstName: String? = null
     private var lastName: String? = null
+    private var middleName: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         var Intent = intent
         firstName = Intent.getStringExtra("First")
         lastName = Intent.getStringExtra("Last")
+        middleName = Intent.getStringExtra("Middle")
         if (firstName.isNullOrBlank() || lastName.isNullOrBlank()) {
             Toast.makeText(this@MainActivity2, "Empty string received", Toast.LENGTH_SHORT).show()
         } else {
             //Get the text views where we will display names
             viewLogin = findViewById(R.id.login)
-            viewLogin!!.text = firstName + " " + lastName + " is logged in!"
+            viewLogin!!.text = firstName + " " + middleName +" "+ lastName + " is logged in!"
         }
+        if(savedInstanceState != null){
+            firstName = savedInstanceState.getString("FirstName")
+            lastName = savedInstanceState.getString("LastName")
+            middleName = savedInstanceState.getString("MiddleName")
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        //Put them in the outgoing Bundle
+        outState.putString("FirstName", firstName)
+        outState.putString("LastName", lastName)
+        outState.putString("MiddleName", middleName)
+
     }
 }
