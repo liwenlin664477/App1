1d7f2278aa7a2c9ad8357f0d7a2824464a865949
WENLIN LI <u1327012@umail.utah.edu>
2023-04-27 20:58:14 -0600

    Added some tests

--- app/src/main/java/com/App1/myapplication/MainActivity.kt
+++ app/src/main/java/com/App1/myapplication/MainActivity.kt
@@ -1,32 +1,23 @@
 package com.App1.myapplication
 
-import android.app.Activity
 import android.content.ActivityNotFoundException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
-import android.content.pm.ActivityInfo
 import android.provider.MediaStore
 import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Build
 import android.view.View
 import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
-import android.os.Environment
-
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var firstName: String? = null
     private var lastName: String? = null
-    private var middleName: String? = null
     private var submitButton: Button? = null
     private var viewFirstName: EditText? = null
-    private var viewMiddleName: EditText? = null
     private var viewLastName: EditText? = null
     private var pictureButton: Button? = null
-    private var pictureView: Bitmap?= null
-    private var picView: ImageView? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,7 +25,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         submitButton!!.setOnClickListener(this)
         pictureButton = findViewById(R.id.button_2)
         pictureButton!!.setOnClickListener(this)
-        picView = findViewById(R.id.image_1)
+
     }
 
     override fun onClick(view: View) {
@@ -44,10 +35,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 //First, get the string from the EditText
                 viewFirstName = findViewById(R.id.firstName)
                 viewLastName = findViewById(R.id.lastName)
-                viewMiddleName = findViewById(R.id.middleName)
                 firstName = viewFirstName!!.text.toString()
                 lastName = viewLastName!!.text.toString()
-                middleName = viewMiddleName!!.text.toString()
+
 
                 //Check if the EditText string is empty
                 if (firstName.isNullOrBlank() || lastName.isNullOrBlank()) {
@@ -62,50 +52,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     val messageIntent = Intent(this, MainActivity2::class.java)
                     messageIntent.putExtra("First", firstName)
                     messageIntent.putExtra("Last", lastName)
-                    messageIntent.putExtra("Middle",middleName)
                     this.startActivity(messageIntent)
                 }
             }
             R.id.button_2 -> {
                 val pictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
                 try{
-                    cameraActivity.launch(pictureIntent)
+                    startActivityForResult(pictureIntent,100)
                 }catch (e: ActivityNotFoundException){
-                Toast.makeText(this,"Error: "+ e.localizedMessage,Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Error: "+ e.localizedMessage,Toast.LENGTH_SHORT)
                 }
             }
         }
     }
 
-    private val cameraActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
-        if(result.resultCode == RESULT_OK) {
-            pictureView = result.data!!.extras!!["data"] as Bitmap?
-            if (isExternalStorageWritable) {
-                picView!!.setImageBitmap(pictureView)
-            }
-            else{
-                Toast.makeText(this, "External storage not writable.", Toast.LENGTH_SHORT).show()
-            }
-
-
-        }
-    }
-
-    private val isExternalStorageWritable: Boolean
-        get() {
-            val state = Environment.getExternalStorageState()
-            return Environment.MEDIA_MOUNTED == state
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            val viewImage : ImageView = findViewById(R.id.image_1)
+            val bitmap = data?.extras?.get("data") as Bitmap
+            viewImage.setImageBitmap(bitmap)
         }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelable("pic", pictureView)
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-
-        super.onRestoreInstanceState(savedInstanceState)
-        pictureView = savedInstanceState.getParcelable("pic");
-        picView!!.setImageBitmap(pictureView)
-    }
 }
